41cf09780a5e3decb6bca47bb3a5064ffe7a472b
Naren <narendran275@gmail.com>
2020-08-22 00:14:00 +0530

    Maven

---
+++ Maven/MavenJavaApp/src/main/java/com/build/maven/java/App.java
@@ -0,0 +1,17 @@
+package com.build.maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String getHelloWorld()
+    {
+        return "Hello World!";
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
